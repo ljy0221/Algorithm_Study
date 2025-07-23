475c13c5c60ff768e20c446e698574adef2ba282
ljy0221 <42925743+ljy0221@users.noreply.github.com>
2025-07-23 12:01:40 +0900

    [Silver I] Title: 트리 순회, Time: 64 ms, Memory: 11592 KB -BaekjoonHub

--- Java/백준/Silver/1991. 트리 순회/트리 순회.java
+++ Java/백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,7 +1,9 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-// 이진 트리 노드 클래스
+/*
+ * 
+ */
 class TreeNode {
     char data;
     TreeNode left;
@@ -15,46 +17,16 @@ class TreeNode {
 }
 
 public class Main {
-    
-    // 전위 순회 (루트 -> 왼쪽 -> 오른쪽)
-    public static void preorder(TreeNode node, StringBuilder result) {
-        if (node != null) {
-            result.append(node.data);
-            preorder(node.left, result);
-            preorder(node.right, result);
-        }
-    }
-    
-    // 중위 순회 (왼쪽 -> 루트 -> 오른쪽)
-    public static void inorder(TreeNode node, StringBuilder result) {
-        if (node != null) {
-            inorder(node.left, result);
-            result.append(node.data);
-            inorder(node.right, result);
-        }
-    }
-    
-    // 후위 순회 (왼쪽 -> 오른쪽 -> 루트)
-    public static void postorder(TreeNode node, StringBuilder result) {
-        if (node != null) {
-            postorder(node.left, result);
-            postorder(node.right, result);
-            result.append(node.data);
-        }
-    }
-    
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        
-        // 노드 개수 입력
-        int n = Integer.parseInt(br.readLine());
-        
-        // 노드들을 저장할 HashMap
-        Map<Character, TreeNode> nodes = new HashMap<>();
-        
-        // 각 노드 정보 입력 및 노드 생성
-        for (int i = 0; i < n; i++) {
-            String[] nodeInfo = br.readLine().split(" ");
+	static int N;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		
+		Map<Character, TreeNode> nodes = new HashMap<>();
+		
+		for(int i = 0; i < N; i++) {
+			String[] nodeInfo = br.readLine().split(" ");
             char parent = nodeInfo[0].charAt(0);
             char leftChild = nodeInfo[1].equals(".") ? '\0' : nodeInfo[1].charAt(0);
             char rightChild = nodeInfo[2].equals(".") ? '\0' : nodeInfo[2].charAt(0);
@@ -79,9 +51,9 @@ public class Main {
                 }
                 nodes.get(parent).right = nodes.get(rightChild);
             }
-        }
-        
-        // 루트 노드는 항상 'A'
+		}
+		
+		// 루트 노드는 항상 'A'
         TreeNode root = nodes.get('A');
         
         // 각 순회 결과 저장할 StringBuilder
@@ -100,5 +72,34 @@ public class Main {
         System.out.println(postorderResult.toString());
         
         br.close();
-    }
+	}
+	
+	// 루 왼 오
+	static void preorder(TreeNode node, StringBuilder result) {
+		if(node != null) {
+			result.append(node.data);
+			preorder(node.left, result);
+			preorder(node.right, result);
+		}
+		
+	}
+	
+	// 왼 루 오
+	static void inorder(TreeNode node, StringBuilder result) {
+		if(node != null) {
+			inorder(node.left, result);
+			result.append(node.data);
+			inorder(node.right, result);
+		}
+	}
+	
+	// 왼 오 루
+	static void postorder(TreeNode node, StringBuilder result) {
+		if(node != null) {
+			postorder(node.left, result);
+			postorder(node.right, result);		
+			result.append(node.data);
+		}
+	}
+
 }
