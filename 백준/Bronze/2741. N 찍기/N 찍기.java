3f5349a84536aa5438c862a5d9d5a7e740c7d3f9
ljy0221 <42925743+ljy0221@users.noreply.github.com>
2025-09-16 11:25:50 +0900

    [Bronze V] Title: N 찍기, Time: 1040 ms, Memory: 34468 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            System.out.println(i);
+        }
+    }
+}
