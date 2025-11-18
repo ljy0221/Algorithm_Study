57b466b0b390e92bc9fc5800cceba841fbb9fe77
ljy0221 <42925743+ljy0221@users.noreply.github.com>
2025-11-19 08:23:50 +0900

    [Bronze V] Title: A+B - 2, Time: 64 ms, Memory: 11516 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+
+        System.out.println(A+B);
+    }
+}
